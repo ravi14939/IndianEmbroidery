b889d4386b76db75b5da9144abce5919b68c0c68
Ravi-kpr <ravikumar14939@gmail.com>
2024-10-01 14:54:50 +0530

    pagelayout_design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenusComponent } from "./menus/menus.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenusComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
