bd18c51bdeb8a5f85336471bb99a45695a8539f8
Ravi-kpr <ravikumar14939@gmail.com>
2024-10-01 14:59:38 +0530

    routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'Home',pathMatch:'full'},
+    {path:'Home',component:HomeComponent},
+    {path:'About',component:AboutComponent},
+    {path:'Contact',component:ContactComponent}
+];
